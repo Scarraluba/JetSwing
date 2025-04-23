029acf9cca1cc7906008ffb79b3e3a1279ccf92d
Scarraluba <younglubah@gmail.com>
2025-04-23 19:15:50 +0200

    Initial Commit

--- src/main/java/concrete/goonie/ui/Fragment.java
+++ src/main/java/concrete/goonie/ui/Fragment.java
@@ -5,19 +5,35 @@ import concrete.goonie.core.NavigationController;
 
 import javax.swing.*;
 
-public abstract class Fragment extends AnimatablePanel  implements NavigableComponent {
+public abstract class Fragment extends AnimatablePanel implements NavigableComponent {
     protected NavigationController navigationController;
-    
+
+    private final String title;
+
+    public Fragment() {
+        this.title = "";
+    }
+
+    protected Fragment(String title) {
+        this.title = title;
+    }
+
     @Override
     public void onAttached() {
         // Default implementation does nothing
     }
-    
+
     @Override
     public void onDetached() {
         // Default implementation does nothing
     }
-    
+
+    @Override
+    public String getDestinationId() {
+        return title.isEmpty() ? getClass().getSimpleName() : title;
+    }
+
+
     public void setNavigationController(NavigationController controller) {
         this.navigationController = controller;
     }
